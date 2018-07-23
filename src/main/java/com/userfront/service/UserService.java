bc7be76ecbb414449d83b657b92a1f0d63d78f55
adaxing <xing2@ualberta.ca>
2018-07-22 21:10:26 -0600

    profile edit adding..

--- src/main/java/com/userfront/service/UserService.java
+++ src/main/java/com/userfront/service/UserService.java
@@ -14,4 +14,6 @@ public interface UserService {
 	
 	void save (User user);
 	User createUser(User user, Set<UserRole> userRoles);
+	
+	User saveUser(User user);
 }
--- src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
+++ src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
@@ -97,6 +97,9 @@ public class UserServiceImpl implements UserService{
 			return false;
 		}
 	}
+	public User saveUser(User user) {
+		return userDao.save(user);
+	}
 	
 
 }
